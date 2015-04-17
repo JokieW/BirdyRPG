58e8d8d8cf2c7ae56b80373ca80e6b298d7604b5
JokieW <gulliverjko@gmail.com>
2015-04-16 20:34:39 -0400

    Added the Altimeter for the fighters

---
+++ Assets/src/Altimeter.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Altimeter : MonoBehaviour 
+{
+    public Vector3 FloorLevel;
+    public Vector3 CeilingLevel;
+
+    private Vector3 _currentLevel;
+    public Vector3 CurrentLevel
+    {
+        get
+        {
+            return CurrentLevel;
+        }
+    }
+
+    private Altitude _currentAltitude;
+    public Altitude CurrentAltitude
+    {
+        get
+        {
+            return _currentAltitude;
+        }
+        set
+        {
+            _currentAltitude = value;
+            Vector3 segments = (CeilingLevel - FloorLevel) / (AltCount - 1);
+            _currentLevel = segments * (int)value;
+
+        }
+    }
+
+    public void GoUp()
+    {
+        if ((int)_currentAltitude < AltCount - 1)
+        {
+            _currentAltitude++;
+        }
+    }
+
+    public void GoDown()
+    {
+        if ((int)_currentAltitude > 0)
+        {
+            _currentAltitude--;
+        }
+    }
+
+
+    public enum Altitude
+    {
+        Ground = 0,
+        Middle = 1,
+        High = 2
+    }
+
+    private int AltCount
+    {
+        get
+        {
+            return Enum.GetValues(typeof(Altitude)).Length;
+        }
+    }
+}
--- Assets/src/Fighter.cs
+++ Assets/src/Fighter.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(TargetPicker))]
+[RequireComponent(typeof(Inventory))]
+[RequireComponent(typeof(Statuses))]
+[RequireComponent(typeof(Stats))]
+[RequireComponent(typeof(Altimeter))]
 public class Fighter : MonoBehaviour 
 {
 

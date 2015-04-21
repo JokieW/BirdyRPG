525c67e29a9ea07d100048c32aca635e5f531b64
JokieW <gulliverjko@gmail.com>
2015-04-20 20:12:04 -0400

    Added status getters

--- Assets/src/Statuses.cs
+++ Assets/src/Statuses.cs
@@ -1,12 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Statuses : MonoBehaviour 
 {
     [SerializeField]
     public List<Ditto> _statuses = new List<Ditto>();
-    
+
+    public bool HasEffect<T>()
+    {
+        return _statuses.Any(x => x.GetType() == typeof(T));
+    }
+
+    public T GetEffect<T>()
+    {
+        return (T)(object)_statuses.FirstOrDefault(x => x.GetType() == typeof(T));
+    }
+
+    public T[] GetEffect<T>()
+    {
+        return (T[])(object)_statuses.All(x => x.GetType() == typeof(T));
+    }
+
     void Update()
     {
         foreach (Ditto d in new List<Ditto>(_statuses))

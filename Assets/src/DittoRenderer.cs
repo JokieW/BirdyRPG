0fd769c4d71d3d6f516b2ce0bd89a6914441510d
JokieW <gulliverjko@gmail.com>
2015-04-28 22:46:33 -0400

    Started inventory wheel

---
+++ Assets/src/DittoRenderer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DittoRenderer : MonoBehaviour 
+{
+    private Ditto _ditto;
+    public Ditto Ditto
+    {
+        get
+        {
+            return _ditto;
+        }
+        set
+        {
+            _ditto = value;
+
+        }
+    }
+
+}
---
+++ Assets/src/InventoryWheel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryWheel : MonoBehaviour 
+{
+    private Fighter _fighter;
+    public Fighter CurrentFighter
+    {
+        get
+        {
+            return _fighter;
+        }
+        set
+        {
+            _fighter = value;
+            ResetWheelTo(value);
+            
+        }
+    }
+
+    public Vector3 Center;
+
+
+    void Update()
+    {
+
+    }
+
+    private ResetWheelTo(Fighter fight)
+    {
+
+    }
+
+}
